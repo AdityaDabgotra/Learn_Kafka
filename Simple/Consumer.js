978037f159df3b54dd96b5dd8ee5ceebc285508f
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-09-24 18:03:35 +0530

    feat: add Kafka consumer to listen for messages on 'rider-updates' topic

---
+++ Simple/Consumer.js
@@ -0,0 +1,18 @@
+const {kafka} = require('./client');
+
+async function init(){
+    const consumer = kafka.consumer({ groupId: 'user-1' });
+    await consumer.connect();
+
+    await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value.toString(),
+                headers: message.headers
+            });
+        },   
+    });
+
+}
